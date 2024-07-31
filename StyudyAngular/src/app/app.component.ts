d32141a9896a66f8bcc2da2f5dc87c70a2832ae6
isac <isacblood@gmail.com>
2024-07-31 19:04:26 -0300

    Projeto-Changes

--- StyudyAngular/src/app/app.component.ts
+++ StyudyAngular/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  inputText = "Texto Inicial Alterado";
+  inputType = "text";
+  isDisabled = true;
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+
+  disableInput() {
+    this.isDisabled = true;
+  }
+
+  setPasswordTypeInput() {
+    this.inputType = "password";
+  }
+
+  setTextTypeInput() {
+    this.inputType = "text";
+  }
+  logInputText() {
+    console.log(this.inputText);
+  }
+
+  handleInputKeyup(event: KeyboardEvent) {
+    const currentText = (event.target as HTMLInputElement).value;
+    console.log(currentText);
+  }
+
+  handleInputEvent(event: any) {
+    console.log(event);
+  }
 }
